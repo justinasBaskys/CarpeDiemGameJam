0a2b600050c1e998f9fdccda0282ee9b2302a1f7
Vari <tmarksinbox@gmail.com>
2020-10-15 16:40:35 +0100

    Added multiple scripts
    Modified:
    Enemy class is inherited now
    Enemy classes inherit from enemy
    Added:
    Shooting script
    Bullet script

---
+++ CarpeDiemNNI/Assets/Scripts/Cannibalized Scrips/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+
+    }
+}
