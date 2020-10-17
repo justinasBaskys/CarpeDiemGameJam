c6296f6bd30b34ff524d5feb08a639d54d470e6a
Vari <tmarksinbox@gmail.com>
2020-10-17 15:34:13 +0100

    Self-Contained
    Redone:
    - Better camera colliders (used less efficient box colliders but easier to manage.)
    Added:
    - Self-Contained box, where upon exiting, enemy and pickup items are deleted.

---
+++ CarpeDiemNNI/Assets/Scripts/Destructor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructor : MonoBehaviour
+{
+    public string[] DestroyTags;
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        foreach(string tag in DestroyTags)
+        if (other.tag == tag)
+        {
+            Debug.Log("Enemy Collision");
+            Destroy(other.gameObject);
+        }
+    }
+}
